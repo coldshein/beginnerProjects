343ddaf6b2336b4bc499698b6b06010d98f2bdfe
coldshein <coldshein@gmail.com>
2022-10-13 00:45:32 +0300

    currency converter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('UAH');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+  const ratesRef = React.useRef({});
+  useEffect( () => {
+    fetch('https://cdn.cur.su/api/latest.json')
+    .then(res => res.json())
+    .then(json => {
+      ratesRef.current = json.rates;
+      onChangeToPrice(1);
+    }).catch((err) => {
+      console.warn(err);
+      alert('Failed to recieve info');
+    });
+  }, []);
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result);
+    setFromPrice(value);
+    
+  }
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result);
+    setToPrice(value);
+  }
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency])
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency])
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+      value={fromPrice} 
+      currency={fromCurrency} 
+      onChangeCurrency={setFromCurrency} 
+      onChangeValue={onChangeFromPrice}/>
+
+      <Block 
+      value={toPrice} 
+      currency={toCurrency} 
+      onChangeCurrency={setToCurrency} 
+      onChangeValue={onChangeToPrice} />
     </div>
   );
 }
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['UAH', 'USD', 'EUR', 'GBP'];
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">

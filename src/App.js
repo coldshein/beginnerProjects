d2b887eb5da5947a2a9783071501635402565a06
coldshein <coldshein@gmail.com>
2022-10-12 23:37:58 +0300

    users list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,49 @@ import React from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import { useState } from 'react';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
+      setUsers(json.data);
+    }).catch(err =>{
+      console.warn(err);
+      alert('error');
+    }).finally(() => setLoading(false));
+  }, [])
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value)
+  }
+
+  const onClickInvite = (id) => {
+    if(invites.includes(id)){
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id])
+    }
+  }
+  const onClickSendInvites = () => {
+    setSuccess(true)
+  }
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {
+        success ? (<Success count={invites.length}/> )
+         :
+         (
+          <Users onClickSendInvites={onClickSendInvites} invites={invites} onClickInvite={onClickInvite} searchValue = {searchValue} onChangeSearchValue={onChangeSearchValue} items={users} isLoading={isLoading}/>
+         )
+      }
+      
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,9 +4,9 @@ export const Success = ({ count }) => {
   return (
     <div class="success-block">
       <img src="/assets/success.svg" alt="Success" />
-      <h3>Успешно!</h3>
-      <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <h3>Success!</h3>
+      <p>All from {count} users has sent invite.</p>
+      <button onClick={ () => window.location.reload()} className="send-invite-btn">Back</button>
     </div>
   );
 };
